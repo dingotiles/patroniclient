06497ae1b8bb1f457f71f328c62f4e3d601a9b84
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-11 09:20:56 +1000

    Fix variable names after search/replace

--- datastructs/structs_test.go
+++ datastructs/structs_test.go
@@ -2,9 +2,9 @@ package datastructs
 
 import "testing"
 
-func TestStructs_NewDataServiceMember(t *testing.T) {
+func TestDataStructs_NewDataServiceMember(t *testing.T) {
 	t.Parallel()
-	DataServiceMemberStr := `
+	dataServiceMemberStr := `
     {
     "key": "/service/025ea0b0-710e-4da2-890d-f245a4d35259/members/49a91b69-264b-4df9-a836-523072b2f778",
     "value": "{\"role\":\"master\",\"state\":\"running\",\"conn_url\":\"postgres://appuser:rminKTk9kOLWWlvh@10.244.21.7:32768/postgres\",\"api_url\":\"http://10.244.21.7:32769/patroni\",\"xlog_location\":6593446208}",
@@ -14,18 +14,18 @@ func TestStructs_NewDataServiceMember(t *testing.T) {
     "createdIndex": 100148
   }
   `
-	DataServiceMember, err := NewDataServiceMember(DataServiceMemberStr)
+	dataServiceMember, err := NewDataServiceMember(dataServiceMemberStr)
 	if err != nil {
 		t.Fatalf("NewDataServiceMember error: %v", err)
 	}
 
 	expectedKey := "/service/025ea0b0-710e-4da2-890d-f245a4d35259/members/49a91b69-264b-4df9-a836-523072b2f778"
-	if DataServiceMember.Key != expectedKey {
+	if dataServiceMember.Key != expectedKey {
 		t.Fatalf("Service member key should be %s", expectedKey)
 	}
 
 	expectedRole := "master"
-	if DataServiceMember.Value.Role != expectedRole {
+	if dataServiceMember.Value.Role != expectedRole {
 		t.Fatalf("Service member role should be %s", expectedRole)
 	}
 }
